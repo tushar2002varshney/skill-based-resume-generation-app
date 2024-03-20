98ab719e67ebb959e28a894ead27b8446804f6b2
tushar2002varshney <tushar.varshney810@gmail.com>
2024-03-19 21:07:23 -0700

    edit html and css code

--- src/app/components/demo-form/demo-form.component.ts
+++ src/app/components/demo-form/demo-form.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './demo-form.component.css'
 })
 export class DemoFormComponent {
+  constructor() { }
 
-}
+  selectTemplate(template: string) {
+    console.log('Selected Template:', template);
+  }
+  }
